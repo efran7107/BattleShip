3dfeca5a470c0d91a3a5127f80a14b05e93df783
Ernie <efran7107@gmail.com>
2023-08-02 20:30:53 -0600

    stopped for the night

--- part-1.js
+++ part-1.js
@@ -4,14 +4,16 @@ var readlineSync = require('readline-sync');
 readlineSync.keyInPause(`Press Any Key to continue...`);
 console.clear();
 
-const gameBoard = new Set(['A1','A2', 'A3', 'B1','B2', 'B3', 'C1','C2', 'C3']);
-const hasPlayed = new Set();
+let gameBoard = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3'];
+let hasPlayed = [];
 
-const num1 = Math.floor(Math.random()* 9);
-const num2 = Math.floor(Math.random()* 9);
+const num1 = Math.floor(Math.random() * 9);
+const num2 = Math.floor(Math.random() * 8);
 
 const boat1 = gameBoard[num1];
+gameBoard.splice(gameBoard.indexOf(boat1), 1);
 const boat2 = gameBoard[num2];
+gameBoard.push(boat1);
 
 let boats = 2;
 
@@ -19,19 +21,21 @@ let gameOver = false;
 
 
 
-const gameLog = () => {console.log(
-`     1    2    3
- A | ${gameBoard[0]} | ${gameBoard[1]} | ${gameBoard[2]} |
- B | ${gameBoard[3]} | ${gameBoard[4]} | ${gameBoard[5]} |
- C | ${gameBoard[6]} | ${gameBoard[7]} | ${gameBoard[8]} |`
-);} 
+const gameLog = () => {
+    console.log(
+        `    1   2   3
+ A |   |   |   |
+ B |   |   |   |
+ C |   |   |   |`
+    );
+}
 
 const location = () => {
     let valid = false;
-    while(!valid) {
+    while (!valid) {
         let loc = readlineSync.question('Enter a location to strike ie \'A2\'');
         loc = loc.charAt(0).toUpperCase() + loc.slice(1);
-        if(loc.length > 2 || (gameBoard.has(loc) === false && hasPlayed.has(loc) === false)){
+        if (loc.length > 2 || (gameBoard.includes(loc) === false && hasPlayed.includes(loc) === false)) {
             console.log('Please enter a valid location');
             valid = false;
         }
@@ -40,19 +44,59 @@ const location = () => {
 }
 
 const launch = (loc) => {
-    if(loc === boat1 || loc === boat2){
-        boat--;
-        console.log(`Hit you have sunk a battleship. ${boats} ship remaining`);
+    if (loc === boat1 || loc === boat2) {
+        if (hasPlayed.includes(loc) === true) {
+            return 1;
+        } else {
+            hasPlayed.push(loc);
+            gameBoard.splice(gameBoard.indexOf(loc), 1);
+            return 2;
+        }
+    } else if (hasPlayed.includes(loc) === true) {
+        return 3;
+    } else {
+        hasPlayed.push(loc);
+        gameBoard.splice(gameBoard.indexOf(loc), 1);
+        return 4
     }
+
+
+
 }
 
-
-
-while (!gameOver){
+while (!gameOver) {
     gameLog();
-    console.log(location());
-    console.clear();
+    const target = location();
+    const hasHit = launch(target);
+
+    switch (hasHit) {
+        case 1:
+            console.log('you have already hit this target. Miss.');
+            break;
+        case 2:
+            boats--;
+            console.log(`Hit. You have sunk a battleship. ${boats} remaining.`);
+            break;
+        case 3:
+            console.log(`You have already picked this location. Miss!`);
+            break;
+        case 4:
+            console.log('You have missed!')
+            break;
+    }
+
+    gameOver = boats === 0 ? true : false;
+    if (gameOver) {
+        playAgain = readlineSync.keyInYN('You have destroyed all battleships. Would you like to play again? ');
+        if (playAgain) {
+            boats = 2;
+            gameBoard = gameBoard.concat(hasPlayed);
+            hasPlayed = [];
+            gameOver = false;
+        } else {
+            console.exit();
+        }
+    }
+
+    const lastTarget = target;
 }
-
-
-
